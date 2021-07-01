9689048f2ca8409fb2605e1e445a268d6d3e4316
mhelms44 <mhelms92@gmail.com>
2021-07-01 07:54:02 -0400

    ex40 objects and classes

---
+++ ex40.py
@@ -0,0 +1,7 @@
+from ex40b import MyStuff
+import ex40a
+ex40a.apple()
+print(ex40a.tangerine)
+thing = MyStuff()
+thing.grape
+print(thing.pear)
---
+++ ex40b.py
@@ -0,0 +1,6 @@
+class MyStuff(object):
+    def __int__(self):
+        self.pear = "And now a thousand years between"
+
+    def grape(self):
+        print("I AM CLASSY GRAPES!")
